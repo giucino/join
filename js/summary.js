9b96e47725c17afb716babaec6ff88e42dfa2d7d
Igor Stoleski <72672441+IgorStoleski@users.noreply.github.com>
2023-08-13 22:47:30 +0200

    add mobile greetings animation

--- js/summary.js
+++ js/summary.js
@@ -1,16 +1,37 @@
-function getGreeting() {
-    const dayTime = document.getElementById('day-time');
-    dayTime.innerHTML = '';
-    const currentHour = new Date().getHours();
+document.addEventListener('DOMContentLoaded', function() {
+  getGreeting('day-time');
+  getGreeting('mobile-day-time');
+});
 
-  
-    if (currentHour >= 5 && currentHour < 12) {
-      return dayTime.innerHTML = "Good morning, ";
-    } else if (currentHour >= 12 && currentHour < 18) {
-      return dayTime.innerHTML = "Good afternoon, ";
-    } else {
-      return dayTime.innerHTML = "Good evening, ";
-    }
+function getGreeting(id) {
+  const dayTime = document.getElementById(id);
+  const currentHour = new Date().getHours();
+
+  if (currentHour >= 5 && currentHour < 12) {
+    dayTime.innerHTML = "Good morning, ";    
+  } else if (currentHour >= 12 && currentHour < 18) {
+    dayTime.innerHTML = "Good afternoon, ";    
+  } else {
+    dayTime.innerHTML = "Good evening, ";    
   }
+}
   
-  
+function toggleActiveClass() {
+  const greetingsMobile = document.getElementById('greetings-mobile');
+  const mobileContainer = document.getElementById('mobile-container');
+  if (window.innerWidth < 1400) {
+    greetingsMobile.classList.add('active-mobile');
+    setTimeout(() => {
+      greetingsMobile.classList.add('d-none');
+      mobileContainer.classList.add('d-none');
+    }, 4000);
+  } else {
+    greetingsMobile.classList.remove('active-mobile');
+    greetingsMobile.classList.remove('d-none');
+    mobileContainer.classList.remove('d-none');
+  }
+}
+
+toggleActiveClass();
+
+window.addEventListener('resize', toggleActiveClass);
