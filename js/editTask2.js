7ddea40f27186d76623558bcb5ccf6894cf5c979
giucino <125276491+giucino@users.noreply.github.com>
2023-10-04 00:13:23 +0200

    changes

--- js/editTask2.js
+++ js/editTask2.js
@@ -213,7 +213,6 @@ function editEditedSubtask(i) {
     if (subtaskContainer) {
         addEditingClasses(subtaskContainer);
     }
-    addSubtaskToEdit(i);
 }
 
 
@@ -260,44 +259,6 @@ function setDisplay(element, value) {
         element.style.display = value;
     }
 }
-
-
-// /**
-//  * Adds editing classes and styles to a given container element.
-//  * - Adds "editing-mode" and "no-hover" classes.
-//  * - Updates the border at the bottom of the container.
-//  * - Adjusts display properties for various child elements of the container such as ".subtask-dot",
-//  *   ".save-subtask-button", ".edit-delete-subtask-button", and ".separator3".
-//  * 
-//  * @param {HTMLElement} container - The container element to modify.
-//  */
-
-// function addEditingClasses(container) {
-//     container.classList.add("editing-mode");
-//     container.classList.add("no-hover");
-//     container.style.borderBottom = "1px solid #4589FF";
-
-//     let dot = container.querySelector(".edit-subtask-dot");
-//     let saveButton = container.querySelector(".edit-save-subtask-button");
-//     let cancelButton = container.querySelector(".edit-edit-delete-subtask-button");
-//     let separator3 = container.querySelector(".separator3");
-
-//     if (dot) {
-//         dot.style.display = "none";
-//     }
-
-//     if (saveButton) {
-//         saveButton.style.display = "block";
-//     }
-
-//     if (cancelButton) {
-//         cancelButton.style.display = "block";
-//     }
-
-//     if (separator3) {
-//         separator3.style.display = "block";
-//     }
-// }
 
 
 /**
@@ -365,50 +326,12 @@ function removeEditingClasses(container) {
     ['editing-mode', 'no-hover'].forEach(cls => container.classList.remove(cls));
     container.style.borderBottom = "";
 
-    removeClassAndStyle(container.querySelector(".edit-subtask-dot"), "", "display", "inline-block");
-    removeClassAndStyle(container.querySelector(".edit-save-subtask-button"), "", "display", "none");
-    removeClassAndStyle(container.querySelector(".edit-edit-delete-subtask-button"), "", "display", "none");
-    removeClassAndStyle(container.querySelector(".separator3"), "", "display", "none");
+    removeClassAndStyle(container.querySelector(".edit-subtask-dot"), null, "display", "inline-block");
+    removeClassAndStyle(container.querySelector(".edit-save-subtask-button"), null, "display", "none");
+    removeClassAndStyle(container.querySelector(".edit-edit-delete-subtask-button"), null, "display", "none");
+    removeClassAndStyle(container.querySelector(".separator3"), null, "display", "none");
 }
 
-
-// /**
-//  * Removes editing-related classes and styles from a given container element.
-//  *
-//  * @function
-//  * @param {HTMLElement} container - The container element to clean up.
-//  * @returns {void}
-//  *
-//  * @example
-//  * const container = document.querySelector('.some-container');
-//  * removeEditingClasses(container);
-//  */
-// function removeEditingClasses(container) {
-//     container.classList.remove("editing-mode");
-//     container.classList.remove("no-hover");
-//     container.style.borderBottom = "";
-
-//     let dot = container.querySelector(".edit-subtask-dot");
-//     let saveButton = container.querySelector(".edit-save-subtask-button");
-//     let cancelButton = container.querySelector(".edit-edit-delete-subtask-button");
-//     let separator3 = container.querySelector(".separator3");
-
-//     if (dot) {
-//         dot.style.display = "inline-block";
-//     }
-
-//     if (saveButton) {
-//         saveButton.style.display = "none";
-//     }
-
-//     if (cancelButton) {
-//         cancelButton.style.display = "none";
-//     }
-
-//     if (separator3) {
-//         separator3.style.display = "none";
-//     }
-// }
 
 /**
  * Speichert den bearbeiteten Titel des aktuellen Tasks.
